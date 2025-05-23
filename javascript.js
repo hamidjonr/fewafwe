4c3e11ef92bd9861ce0f6005d7802314ea5fe4a9
hamidjonr <144801661+hamidjonr@users.noreply.github.com>
2025-05-23 16:35:06 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,69 @@
+// for(let i = 1; i <= 10; i++){
+//   document.write(i)
+// }
+
+
+
+
+
+
+// let week = +prompt("hafta kunini kiriting")
+
+// switch (week) {
+//   case 1:
+//     document.write("Yanva : Qish")
+//     break;
+//   case 2:
+//     document.write("Fevral : Qish")
+//     break;
+//   case 3:
+//     document.write("Mart : Bahor")
+//     break;
+//   case 4:
+//     document.write("Aprel : Bahor")
+//     break;
+//   case 5:
+//     document.write("May : Bahor")
+//     break;
+//   case 6:
+//     document.write("Iyun : Yoz")
+//     break;
+//   case 7:
+//     document.write("Iyul : Yoz")
+//     break;
+//   case 8:
+//     document.write("Avgust : Yoz")
+//     break;
+//   case 9:
+//     document.write("Sentabr : Kuz")
+//     break;
+//   case 10:
+//     document.write("Oktabr : Kuz")
+//     break;
+//   case 11:
+//     document.write("Noyabr : Kuz")
+//     break;
+//   case 12:
+//     document.write("Dekabr : Qish")
+//     break;
+// }
+
+
+
+
+
+
+// let color = document.getElementById("color");
+// let background = document.getElementById("background");
+
+// color.onclick = function () {
+//   lorem.style.color = "red"
+// }
+
+// background.onclick = function () {
+//   lorem.style.background = "green"
+// }
+
+ 
+
+
